64d66e4d872e69b43feac4bbc83d137cb7e301b9
Sergio Márquez <smarquezs@gmail.com>
2014-06-02 19:34:05 -0400

    ADD: cookbooks

--- chef/site-cookbooks/taller/metadata.rb
+++ chef/site-cookbooks/taller/metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'rbenv'
 depends          'ruby_build'
-depends          'mysql'
+#depends          'mysql'
