3ef8726e669c7ded07433454d32cb988ff4fb4f6
Sergio Márquez <smarquezs@gmail.com>
2014-06-02 22:13:24 -0400

    ADD: sqlite cookbook

--- chef/cookbooks/mysql/recipes/server.rb
+++ chef/cookbooks/mysql/recipes/server.rb
@@ -17,15 +17,35 @@
 # limitations under the License.
 #
 
-mysql_service "default" do
-  version '5.5'
-  port '3306'
-  data_dir '/data'
-  allow_remote_root true
-  remove_anonymous_users false
-  remove_test_database false
-  server_root_password node['mysql']['server_root_password']
-  server_debian_password node['mysql']['server_debian_password']
-  server_repl_password node['mysql']['server_repl_password']
-  action :create
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+::Chef::Recipe.send(:include, Opscode::Mysql::Helpers)
+
+if Chef::Config[:solo]
+  missing_attrs = %w[
+    server_debian_password
+    server_root_password
+    server_repl_password
+  ].select { |attr| node['mysql'][attr].nil? }.map { |attr| %Q{node['mysql']['#{attr}']} }
+
+  unless missing_attrs.empty?
+    Chef::Application.fatal! "You must set #{missing_attrs.join(', ')} in chef-solo mode." \
+    " For more information, see https://github.com/opscode-cookbooks/mysql#chef-solo-note"
+  end
+else
+  # generate all passwords
+  node.set_unless['mysql']['server_debian_password'] = secure_password
+  node.set_unless['mysql']['server_root_password']   = secure_password
+  node.set_unless['mysql']['server_repl_password']   = secure_password
+  node.save
+end
+
+case node['platform_family']
+when 'rhel'
+  include_recipe 'mysql::_server_rhel'
+when 'debian'
+  include_recipe 'mysql::_server_debian'
+when 'mac_os_x'
+  include_recipe 'mysql::_server_mac_os_x'
+when 'windows'
+  include_recipe 'mysql::_server_windows'
 end
