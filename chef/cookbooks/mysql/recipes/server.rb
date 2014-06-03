f8c5df87e06ecc196844a02e5279701decf516e4
Sergio Márquez <smarquezs@gmail.com>
2014-06-02 20:08:04 -0400

    FIX: mysql cookbook

--- chef/cookbooks/mysql/recipes/server.rb
+++ chef/cookbooks/mysql/recipes/server.rb
@@ -17,16 +17,15 @@
 # limitations under the License.
 #
 
-mysql_service node['mysql']['service_name'] do
-  version node['mysql']['version']
-  port node['mysql']['port']
-  data_dir node['mysql']['data_dir']
+mysql_service "default" do
+  version '5.5'
+  port '3306'
+  data_dir '/data'
+  allow_remote_root true
+  remove_anonymous_users false
+  remove_test_database false
   server_root_password node['mysql']['server_root_password']
   server_debian_password node['mysql']['server_debian_password']
   server_repl_password node['mysql']['server_repl_password']
-  allow_remote_root node['mysql']['allow_remote_root']
-  remove_anonymous_users node['mysql']['remove_anonymous_users']
-  remove_test_database node['mysql']['remove_test_database']
-  root_network_acl node['mysql']['root_network_acl']
   action :create
 end
