750cb64eb7ac015cddb16fd41e87c73da2cac6a7
Sergio Márquez <smarquezs@gmail.com>
2014-07-02 14:42:33 -0400

    ADD: locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :locale, :site
+
+  before_action :set_locale
+
+  def set_locale
+    site = params[:site]
+
+    case site
+    when "cl"
+      I18n.locale = :'es-CL'  
+    when "mx"
+      I18n.locale = :'es-MX' 
+    else
+      I18n.locale = :en 
+    end
+    
+  end
+
+  def locale
+    I18n.locale.downcase
+  end
+
+  def site
+    params[:site]
+  end
 end
